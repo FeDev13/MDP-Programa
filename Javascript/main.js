c3e6c7314bbfbe07718eee248514f3925207fd8c
Federico Robledo <federicorobledo85@gmail.com>
2023-07-07 23:19:53 -0300

    agrega ejs con and y or

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,33 +1,38 @@
-//true o false
+//true + true = true
+//true  + false = false
+//false + true = false
+//false + false = false
 
-if (false) {
-  console.log("esto se ejecuta");
-}
+//true + true = true
+//true + false = true
+//false + true = true
+//false + false = false
 
-const num = 12; //comparo con el doble igual ==
+/* let user = prompt("Ingrese su usuario");
+let password = prompt("Ingrese su password");
 
-if (num == 14) {
-  console.log("es verdadero");
-}
+let usuarioGuardado = "Pedro";
 
-let esEstricto = "102" == 103; //cuando usamos igualdad simple o estricta el resultado de la comparacion sera un booleano true o false
+let pass = "5555";
 
-console.log(esEstricto);
-
-const precioProducto = 99;
-
-if (precioProducto < 50) {
-  console.log("el precio es menor a 50");
-} else if (precioProducto < 100) {
-  console.log("el precio es menor a 100");
+if (user === usuarioGuardado && pass === password) {
+  console.log("Bienvenido a la plataforma " + usuarioGuardado);
 } else {
-  console.log("el precio es " + precioProducto);
-}
-
-/* let nombre = prompt("ingrese su nombre");
-
-if (nombre == "") {
-  alert("debe ingresar su nombre");
-} else {
-  alert("Hola " + nombre);
+  console.log("Usuario o password incorrecto");
 } */
+
+let user = prompt("Ingrese usuario");
+let pass = prompt("ingrese password");
+let usuarioGuardado = "Juan";
+
+if (user !== "" && pass !== "") {
+  //primero se chequea que los strings no sean vacios
+  if (user == "juan" || user == "Juan") {
+    //si lo anterior es falso evalua si alguno de los users coinciden
+    console.log("Hola Juan!"); //como esto es or si alguna de las 2 condiciones es true la condicion es verdadera y se ejecuta el bloque de codigo
+  } else {
+    console.log("Hola usuario " + user); //de lo contrario saluda a otro usuario
+  }
+} else {
+  console.log("Faltan datos"); // aca viene solo si la primer condicion es true
+}
