9c0ffe6a5ec97cc549f4d413b27ba291262ca71d
Federico Robledo <federicorobledo85@gmail.com>
2023-07-19 23:08:45 -0300

    agrega clase funciones

---
+++ Javascript/funciones/main.js
@@ -0,0 +1,37 @@
+//declaracion de la funcion
+function saludar() {
+  //cuerpo de la funcion
+  console.log("hola");
+}
+//invocacion o llamada
+saludar();
+
+function suma(num1, num2) {
+  //num1 y num2 son los parametros
+  return num1 + num2;
+}
+console.log(suma(5, 7)); //5 y 7 son los argumentos
+
+function esMayorDeEdad(edad) {
+  if (edad > 18) {
+    return "es mayor de edad";
+  } else {
+    return "es menor de edad";
+  }
+}
+esMayorDeEdad();
+
+let userName = "Juan";
+
+//una funcion puede acceder a una variable global y modificarla
+function mostrarMensaje() {
+  let userName = "Pedro"; //esta variable es de scope local y no se pisa con la variable global
+  let mensaje = "Hola " + userName;
+}
+console.log(userName);
+mostrarMensaje();
+console.log(userName);
+
+//un parametro es una variable listada dentro de los parentesis en la declaracion de funcion, al momento de declararla
+//un argumento es el valor que es pasado a la funcion cuando esta es llamada
+//se declara una funcion pasando argumentos y luego la llamamos pasandole argumentos
